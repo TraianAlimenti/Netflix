e89a219025e43642b6cef102e8f33bbe520e34a5
Fran Laiuppa <franco.laiuppa@gmail.com>
2021-03-19 21:18:57 +0100

    index.ts now creates an express server on port 3000

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-const express = require('express');
-const mockData = require('./tests/mock.json');
+import { createServer } from './server'
 
-const app = express();
-const port = 3000;
-
-app.get("/categories", (req, res) => {
-  res.send(mockData.categories);
-});
-
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+createServer(3000);
