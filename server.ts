bea7399b73aa5d4f4883b07dbd7ba9ed587c9c46
Traian Alimenti Bel <traian.alimentibel@gmail.com>
2021-03-22 12:54:56 -0300

    implementing requests for titles

--- server.ts
+++ server.ts
@@ -49,8 +49,58 @@ app.delete("/categories/:id", (req: any, res: any) => {
     res.json(inMemoryStore[id])
   }
 });
+app.get("/titles", (_req: express.Request, res: express.Response) => {
+  res.send(mockData.titles);
+});
+
+app.get('/titles/:id', (req: express.Request, res: express.Response) => {
+  
+  let id = parseInt(req.params.id) -1
+  res.send(mockData.titles[id])
+
+});
+
+//TODO: isn't adding id if it's not specified in the body
+app.post("/titles", (req: express.Request, res: express.Response) => { 
+  let inputJson = req.body;
+
+  inMemoryStore = Object.values(mockData.titles);
+  let newId = inMemoryStore.length + 1
+
+  inputJson.id = newId
+  inMemoryStore.push(inputJson)
+  res.sendStatus(201)
+});
+
+app.patch("/titles/:id", (req: express.Request, res: express.Response) => {
+  let inputJson = req.body;
+  let id = parseInt(req.params.id) -1
+
+  for(var key in inputJson)
+    mockData.titles[id][key] = inputJson[key]
+
+  inMemoryStore = Object.values(mockData.titles);
+
+  res.json(inMemoryStore[id])
+});
+
+app.delete("/titles/:id", (req: express.Request, res: express.Response) => {
+  let id = parseInt(req.params.id) -1
+  
+  if (mockData.titles[id] == null)
+    res.sendStatus(404)
+  else {
+    mockData.titles.splice(id,1)
+    inMemoryStore = Object.values(mockData.titles);
+    res.json(inMemoryStore[id])
+  }
+});
 
 export const createServer = (port?: number, newDatabaseFilename?: string) => {
+  if (newDatabaseFilename != undefined) 
+    databaseFilename = newDatabaseFilename
+
+  mockData = require(databaseFilename);
   const server = app.listen(port, () => {
     // @ts-ignore Express typings are wrong, this value actually does exist
     if (port) {
