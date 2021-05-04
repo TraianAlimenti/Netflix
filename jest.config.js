bbce0292aadee3d82fb20fc071f79a714187dfac
Fran Laiuppa <franco.laiuppa@gmail.com>
2021-05-04 10:11:39 +0200

    jest.config.js now only runs JS tests 🎉
    Created tests/setup.ts that recreates and initializes the test database. It only runs ONCE before all tests.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.[j]s?(x)", "**/?(*.)+(spec|test).[j]s?(x)"],
+  globalSetup: "<rootDir>/tests/setup.js"
 };
